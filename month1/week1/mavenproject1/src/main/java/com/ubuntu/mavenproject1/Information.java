8cf7eac22f85b508436e7dec7348d4f71e1c99e3
debra <deltaburs@gmail.com>
2025-06-08 13:33:36 +0330

    Some challanges

---
+++ month1/week1/mavenproject1/src/main/java/com/ubuntu/mavenproject1/Information.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ubuntu.mavenproject1;
+
+/**
+ *
+ * @author toor
+ */
+public class Information {
+    public static void main(String[] args){
+        int age = 30;
+        String name = "Farid kaki";
+        double height = 1.78;
+        boolean isStudent = false;
+        
+        System.out.println("Hello my name is " + name + " and i have " + age + " years old and im student: " + isStudent);
+    }
+}
