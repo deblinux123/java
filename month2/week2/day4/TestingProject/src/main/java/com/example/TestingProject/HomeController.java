9e2c477cb7a3534f1c929a00b7160eb38876a49c
debra <deltaburs@gmail.com>
2025-07-27 11:50:39 +0330

    First usage of thyemleaf

---
+++ month2/week2/day4/TestingProject/src/main/java/com/example/TestingProject/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.TestingProject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+@Controller
+public class HomeController 
+{
+    @GetMapping("/")
+    public String home(Model model)
+    {
+        model.addAttribute("message", "This is my first usage of thymeleaf");
+        return "index";
+    }
+}
---
+++ month2/week2/day4/TestingProject/src/test/java/com/example/TestingProject/TestingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
