d300b3c6d43c3cd04630c503a6a1e5114fb9bd18
farid kaki <deltaburs@gmail.com>
2025-08-23 13:27:28 +0330

    Home page html

---
+++ month6/storeDemo/src/main/java/com/example/storeDemo/controller/homeController.java
@@ -0,0 +1,15 @@
+package com.example.storeDemo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController
+{
+    @RequestMapping("/")
+    public String homePage()
+    {
+        return "home";
+    }
+}
